50fa57a3fc33ebf0cc575bb030c20b29ec44470f
schmidtjos <schmidtjos@urbandaleschools.com>
2020-10-21 10:24:37 -0500

    finished jedi training

--- 5.0_Jedi_Training.py
+++ 5.0_Jedi_Training.py
@@ -3,22 +3,29 @@
 '''
  1. Make the following program work.
    '''  
-     print("This program takes three numbers and returns the sum.")
-     total = 0
-
-     for i in range(3):
-         x = input("Enter a number: ")
-         total = total + i
-     print("The total is:", x)
+     # print("This program takes three numbers and returns the sum.")
+     # total = 0
+     #
+     # for i in range(3):
+     #     x = input("Enter a number: ")
+     #     total = total + i
+     # print("The total is:", x)
   
-
+print("This program takes three numbers and returns the sum.")
+total = 0
+for i in range(3):
+    x = int(input("Enter a number: "))
+    total += x
+print("The total is:",total)
 
 '''
   2. Write a Python program that will use a FOR loop to print the even
      numbers from 2 to 100, inclusive.
 '''
 
-
+for i in range(2,101):
+    if i%2==0:
+        print(i)
 
 
 
@@ -28,7 +35,11 @@
      a WHILE loop, don't use a FOR loop.
 '''
 
-
+x = 10
+while x >= 0:
+    print(x)
+    x -= 1
+print("Blast off!")
 
 
 
@@ -36,7 +47,8 @@
 '''
   4. Write a program that prints a random integer from 1 to 10 (inclusive).
 '''
-
+import random
+print(random.randrange(0,11))
 
 
 
@@ -52,3 +64,22 @@
      if statements.
       
 '''
+print("Today I'll be asking you for seven numbers.")
+global sum,pos,neg,nil
+sum = 0
+pos = 0
+neg = 0
+nil = 0
+for i in range(7):
+    num = int(input("Number: "))
+    sum += num
+    if num > 0:
+        pos += 1
+    elif num < 0:
+        neg += 1
+    else:
+        nil += 1
+print("Sum:",sum)
+print("No. of positive answers:",pos)
+print("No. of negative answers:",neg)
+print("No. of zeroes entered:",nil)
