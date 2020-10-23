479715f18634496bd236aaa2e9ec948318520893
schmidtjos <schmidtjos@urbandaleschools.com>
2020-10-23 11:56:54 -0500

    finished roshambo

--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -15,43 +15,58 @@ import random
 
 game = True
 
+uscore = 0
+cscore = 0
+nilscore = 0
+
 while game:
-    user = random.randrange(1,4)
     computer = random.randrange(1,4)
-    uscore = 0
-    cscore = 0
-    nilscore = 0
-    if user == 1:
+    print("What would you like to do?\n")
+    print("If rock, enter 1\nIf paper, enter 2\nIf scissors, enter 3\nIf view score, enter 4\nIf quit, enter 0")
+    print()
+    user = input()
+    if user == "1" or user == "2" or user == "3" or user == "4" or user == "0":     #filters invalid answers
+        user = int(user)
+    else:
+        print("Invalid Answer\n")
+    if user == 1:                           #Sets user's choice
         print("User: Rock")
     elif user == 2:
         print("User: Paper")
-    else:
+    elif user == 3:
         print("User: Scissors")
-    if computer == 1:
-        print("Computer: Rock")
-    elif computer == 2:
-        print("Computer: Paper")
-    else:
-        print("Computer: Scissors")
-    if user == computer:
-        print("Tie")
-        nilscore += 1
-    elif user == 1 and computer == 2:
-        print("You Lost")
-        cscore += 1
-    elif user == 1 and computer == 3:
-        print("You Win!")
-        uscore += 1
-    elif user == 2 and computer == 1:
-        print("You Win!")
-        uscore += 1
-    elif user == 2 and computer == 3:
-        print("You Lose")
-        cscore += 1
-    elif user == 3 and computer == 1:
-        print("You Lose")
-        cscore += 1
-    elif user == 3 and computer == 2:
-        print("You Win!")
-    else:
-        print("come again?")
+    elif user == 4:                             #Checks score
+        print("User:",uscore)
+        print("Computer:",cscore)
+        print("Ties:",nilscore)
+        print()
+    elif user == 0:                             #Exits game
+        print("Exiting game...")
+        game = False
+    if user == 1 or user == 2 or user == 3:
+        if computer == 1:                       #Sets computer's choice
+            print("Computer: Rock\n")
+        elif computer == 2:
+            print("Computer: Paper\n")
+        else:
+            print("Computer: Scissors\n")
+        if user == computer:                    #Determines match results and adjusts score
+            print("Tie\n")
+            nilscore += 1
+        elif user == 1 and computer == 2:
+            print("You Lost\n")
+            cscore += 1
+        elif user == 1 and computer == 3:
+            print("You Won!\n")
+            uscore += 1
+        elif user == 2 and computer == 1:
+            print("You Won!\n")
+            uscore += 1
+        elif user == 2 and computer == 3:
+            print("You Lost\n")
+            cscore += 1
+        elif user == 3 and computer == 1:
+            print("You Lost\n")
+            cscore += 1
+        elif user == 3 and computer == 2:
+            print("You Won!\n")
